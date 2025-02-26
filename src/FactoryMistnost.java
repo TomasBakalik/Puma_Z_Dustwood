131ed56189666c6eaad25aa9c214a6a8cccfb21e
tomas <bakalik@spsejecna.cz>
2025-02-26 15:18:44 +0100

    Třída FactoryMistnost byla přidána a metoda pro vytváření místností též.

---
+++ src/FactoryMistnost.java
@@ -0,0 +1,25 @@
+public class FactoryMistnost {
+
+    public static Mistnost vytvorMistnost(String nazev){
+        switch (nazev.toLowerCase()){
+            case "namesti":
+                return new Namesti();
+            case "zoo":
+                return new Zoo();
+            case "hospoda":
+                return new Hospoda();
+            case "starostuv dum":
+                return new StarostuvDum();
+            case "obchod":
+                return new Obchod();
+            case "ohrada":
+                return new Ohrada();
+            case "souseduv dum":
+                return new SouseduvDum();
+            case "strecha hospody":
+                return new StrechaHospody();
+            default:
+                throw new IllegalArgumentException("Neznama mistnost: " + nazev);
+        }
+    }
+}

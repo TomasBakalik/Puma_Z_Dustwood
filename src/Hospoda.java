a68e1a91dbb604fb46b56c767679ae1aee8392eb
tomas <bakalik@spsejecna.cz>
2025-02-26 15:01:47 +0100

    Třída Hospoda, která je odděděná od třídy místnost byla přidána.

---
+++ src/Hospoda.java
@@ -0,0 +1,5 @@
+public class Hospoda extends Mistnost{
+    public Hospoda(String popis, String nazev) {
+        super("Hospoda", "Mistni hospoda, ve ktere se hrac muze dozvedet informace o pume a cele situaci");
+    }
+}

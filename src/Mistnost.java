a2c4d5e9730ece42a6b6e5c87f5b246a7aae5419
tomas <bakalik@spsejecna.cz>
2025-02-26 14:58:14 +0100

    Abstraktní třída místnost byla přidaná. Byly do ní zároveň přidány hlavní metody a proměnné.

---
+++ src/Mistnost.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public abstract class Mistnost {
+
+    protected String nazev;
+    protected String popis;
+    protected HashMap<String, Mistnost> vychody;
+
+    public Mistnost(String popis, String nazev) {
+        this.popis = popis;
+        this.nazev = nazev;
+        this.vychody = new HashMap<>();
+    }
+
+    public void nastavitVychod(String nazev, Mistnost mistnost){
+        vychody.put(nazev.toLowerCase(), mistnost);
+    }
+
+    public Mistnost ziskatVychod(String nazev){
+        return vychody.get(nazev.toLowerCase());
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+}

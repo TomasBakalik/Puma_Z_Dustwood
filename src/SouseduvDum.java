1657922cddcd42591f0ca37d5bbb67f741729482
tomas <bakalik@spsejecna.cz>
2025-02-26 15:10:22 +0100

    Třída SouseduvDum, která je odděděná od třídy místnost byla přidána.

---
+++ src/SouseduvDum.java
@@ -0,0 +1,5 @@
+public class SouseduvDum extends Mistnost{
+    public SouseduvDum() {
+        super("Souseduv Dum", "Dum soused Clarka, ve kterem hrac zjisti dulezite informace o pume");
+    }
+}
